673e2575ad882f4626a44273f2d509dc3f714bcd
Christopher Moffett <christophmoffett@gmail.com>
2017-02-11 15:09:02 -0800

    Linked Lists and Priority Queues
    Base code for new data structs

---
+++ src/data_structures/OrderedArrayPriorityQueue.java
@@ -0,0 +1,63 @@
+package data_structures;
+
+import java.util.Iterator;
+import java.util.LinkedList; //ERASE
+
+public class OrderedArrayPriorityQueue implements PriorityQueue{
+
+	LinkedList<Integer> list = new LinkedList<>();
+	@Override
+	public boolean insert(Comparable object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Comparable remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Comparable obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/data_structures/UnorderedArrayPriorityQueue.java
@@ -0,0 +1,61 @@
+package data_structures;
+
+import java.util.Iterator;
+
+public class UnorderedArrayPriorityQueue implements PriorityQueue {
+
+	@Override
+	public boolean insert(Comparable object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Comparable remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Comparable obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
